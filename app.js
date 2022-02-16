7f4e38287b4e4d5f9f7fd4ea27f4f40520d8de00
BenFSaks <83617955+BenFSaks@users.noreply.github.com>
2022-02-16 14:30:20 -0500

    deleted log lines

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express')
 const req = require('express/lib/request')
 const path = require('path')
 var app = express()
-var x = path.join(__dirname, '/public')
-console.log(x)
+
 app.set('views', path.join(__dirname, '/public/views'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/signup', (req,res)=>{
@@ -22,3 +21,4 @@ app.get('/', (req,res)=>{
 
 
 app.listen(3001)
+console.log("localhost:3001")
