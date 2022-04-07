993dfeca42ff0a404f9d27b6ce5e0aafe5e3c65e
BenFSaks <83617955+BenFSaks@users.noreply.github.com>
2022-04-06 23:55:39 -0400

    Added a popup menu
    - In addBook.js created add book button
    - added function to make menu appear and disappear

--- public/scripts/addBook.js
+++ public/scripts/addBook.js
@@ -1,14 +1,14 @@
 class Book{
     constructor(coverFile, coverName){
         this.innerHTML = `
-        <div class="book p-2 hover:bg-blue-900">
+        <div class="cursor-pointer p-2 hover:bg-blue-900">
             <img class="w-36 h-52" src="${coverFile}" alt="">
             <h1 class="font-sans text-white text-center">${coverName}</h1>
         </div>
         `
     }
 }
-bookList = []
+let bookList = []
 bookList[0] = {
     coverFile: "../img/georgeorwell1984.png",
     title: "George Orwell 1984"
@@ -17,9 +17,32 @@ bookList[1] = {
     coverFile: "../img/harrypottersorcstonejkrowling.jpg",
     title: "JK Rowling Harry"
 }
-bookList[2] = bookList[0]
+bookList[2] = {
+    coverFile: "../img/turtles.png",
+    title: "John Green Turtles" //All The Way Down"
+}
+bookList[3] = bookList[1]
 var shelf = document.getElementById("shelf")
 bookList.forEach(element => {
     let book = new Book(element.coverFile,element.title)
     shelf.innerHTML += book.innerHTML
 })
+// Add a book, button
+shelf.innerHTML +=
+`
+    <div class="p-2 cursor-pointer">
+        <div id="addBook" class= "w-36 h-52 bg-white hover:bg-gray-200">
+            <h1 class="font-bold text-center text-green-500">Add A Book</h1>
+            <img class="mx-auto mt-8" src="../img/greenplusicon.png">
+        </div>
+    </div>
+`
+document.getElementById("addBook").addEventListener("click", addBookPopUp)
+let menu = document.getElementById("popUpMenu")
+function addBookPopUp(){
+    menu.classList.remove("hidden")
+}
+document.getElementById("menuClose").addEventListener("click", closeMenu)
+function closeMenu(){
+    menu.classList.add("hidden")
+}
--- public/scripts/navBar.js
+++ public/scripts/navBar.js
@@ -5,7 +5,13 @@ class Header extends HTMLElement{
             <p class=" text-black ml-5 text-2xl font-mono hover:bg-red-200">
                 <a href="/">CrossLib:</a>
             </p>
-            <h1 class="text-cyan-300 ml-5 font-mono text-center text-2xl">Place's Cross Library</h1>   
+            <h1 class="text-cyan-300 ml-5 font-mono text-center text-2xl">Place's Cross Library</h1>
+            <div class="text-yellow-700 font-mono text-xl px-0.5 ml-4 flex">
+                <a href="/signup" class=" border-cyan-300 border-r border-t border-l">Signup</a>    
+                <a href="/login" class="border-cyan-300 border-r border-t">login</a>
+                <a href="/shelf" class="border-cyan-300 border-r border-t">Shelf</a>
+
+            </div>   
         </nav>    
         `
     }
